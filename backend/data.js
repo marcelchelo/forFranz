4ddf54897bc351eefc1877df852db0a34c5ed611
Marcelo <marcelo95326@icloud.com>
2021-09-02 16:26:15 -0400

    LAtest
    latest

---
+++ backend/data.js
@@ -0,0 +1,94 @@
+const data = {
+    products : [
+        {
+            _id:'1',
+            name:'El negro del Wasa',
+            category: "Pelicula",
+            image: "/images/p1.jpg", 
+            price: 120,
+            brand: "nike",
+            rating: 4,
+            numReviews: 10,
+            description: "No caminaras una semana ",
+            countInStock:10,
+
+        },
+
+        {
+            _id:'2',
+            name:'Sucio Potter',
+            category: "Pelicula",
+            image: "/images/p2.jpg", 
+            price: 29,
+            brand: "nike",
+            rating: 2.5,
+            numReviews: 10,
+            description: "Varita no incluida",
+            countInStock:120,
+
+
+        },
+
+        {
+            _id:'3',
+            name:'El Bananero',
+            category: "Pelicula",
+            image: "/images/p3.jpg", 
+            price: 20,
+            brand: "nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Gordo Pajero",
+            countInStock:0,
+
+        },
+
+        {
+            _id:'4',
+            name:'Peloconcha',
+            category: "Pelicula",
+            image: "/images/p4.jpg", 
+            price: 520,
+            brand: "NGM",
+            rating: 4.5,
+            numReviews: 10,
+            description: "viejo con 70 años de experiencia",
+            countInStock:120,
+
+
+        },
+
+        {
+            _id:'5',
+            name:'LLego el Lecherooooo',
+            category: "Pelicula",
+            image: "/images/p5.jpg", 
+            price: 220,
+            brand: "nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "vas a ver gotas",
+            countInStock:0,
+
+
+        },
+
+        {
+            _id:'6',
+            name:'Franz Guzman',
+            category: "Pelicula",
+            image: "/images/p6.jpg", 
+            price: 520,
+            brand: "made in Peru",
+            rating: 4.5,
+            numReviews: 15,
+            description: "Te toca el codo que da gusto. ",
+            countInStock:10,
+
+
+        },
+
+
+    ]
+}
+export default data;
